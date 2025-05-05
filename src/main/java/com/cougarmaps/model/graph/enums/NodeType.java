7e5c5d28242f55ab6f536012fc8fd86880ce7c30
JJ <hoffm101@csusm.edu>
2025-05-04 20:33:06 -0700

    Added University Hall

---
+++ src/main/java/com/cougarmaps/model/graph/enums/NodeType.java
@@ -0,0 +1,15 @@
+package com.cougarmaps.model.graph.enums;
+
+/**
+ * Enum representing the different types of nodes in the graph.
+ * Each node type corresponds to a specific location or feature in the building.
+ */
+public enum NodeType {
+    CLASSROOM,  // Represents a classroom
+    RESTROOM,   // Represents a restroom
+    HALLWAY,    // Represents a hallway
+    ELEVATOR,   // Represents an elevator
+    DOOR,       // Represents a door
+    UNKNOWN     // Represents an unknown type
+
+}
